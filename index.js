42ccc393c1659d7d20f050111f50cf5bcfc4b193
nicobazzoni <jillianamagsila@Jillians-iMac.fios-router.home>
2021-08-02 15:12:01 -0400

    commit tree 88b7ff64cc4475d7e15759fd722b96dab643fa53

--- index.js
+++ index.js
@@ -11,3 +11,26 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg;
+}
+
+function greet(the_time){
+let this_time = the_time.split(":");
+let anothert_time = this_time[0].concat(this_time[1])
+let the_result= parseInt(another_time, 10)
+
+if (the_result < 1200) {
+  return "Good Morning";
+
+}
+
+else if (the_result > 1700) {
+  return "Good Evening";
+}
+else {
+  return "Good Afternoon";
+}
+
+}
